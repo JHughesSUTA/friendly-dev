688f992af5795c3df616a43500c49d738f679193
James Hughes <james.a.hughes@gmail.com>
2025-09-02 18:16:43 -0500

    creating routes, fix devtools console error

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,12 +1,13 @@
-import type { Route } from "./+types/home";
+import type { Route } from "./+types/index";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "The Friendly Dev" },
+    { title: "The Friendly Dev | Welcome" },
     { name: "description", content: "Custom website development" },
   ];
 }
 
 export default function Home() {
-  return <>MyApp</>;
+  console.log("hello from home...");
+  return <section>My App</section>;
 }
